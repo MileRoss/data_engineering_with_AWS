078e0e874e9fc067d33770cba3349cd77984911d
milenko <1307mile@gmail.com>
2025-05-22 21:01:45 +0200

    reorganized folders

--- 4_automate_data_pipelines/4.1_airflow_dag/start_airflow.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/opt/airflow/start-services.sh
-/opt/airflow/start.sh
-airflow users create --email student@example.com --firstname aStudent --lastname aStudent --password admin --role Admin --username admin
-airflow scheduler
